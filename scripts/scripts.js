a5e745862892dbe67a86af9962cf1cdf8766084a
Juls Lin <julianna.lin4@gmail.com>
2023-04-06 22:35:47 -0700

    text on button updated from dark theme to light theme

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,14 @@ const darkthemeBG = document.querySelector('.theme')
 darkthemeBG.addEventListener("click", () => {
     body.classList.toggle("dark-theme");
     nav.classList.toggle("nav-dark-theme");
-
 });
 
+let buttonRef = document.querySelector('.theme')
+function changeText () {
+    if (buttonRef.textContent === "Dark Theme"){
+        buttonRef.textContent = "Light Theme";
+    } else {
+        buttonRef.textContent = "Dark Theme"
+    }
+}
+buttonRef.addEventListener("click", changeText);
