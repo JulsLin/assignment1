56d19e392244914873b1079ace66b43a43a82f94
Juls Lin <julianna.lin4@gmail.com>
2023-04-07 20:27:25 -0700

    show prompt to the user and asik for the title of the note

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,6 @@
+// When the "Dark Theme" button is clicked, background colors should change to darker 
+//(use 2 different colors for the left and right parts)
+//use toggling class properties
 const body = document.querySelector('body')
 const nav = document.querySelector('nav')
 const darkthemeBG = document.querySelector('.theme')
@@ -7,6 +10,7 @@ darkthemeBG.addEventListener("click", () => {
     nav.classList.toggle("nav-dark-theme");
 });
 
+//text on the button should be updated to "Light Theme"
 let buttonRef = document.querySelector('.theme')
 function changeText () {
     if (buttonRef.textContent === "Dark Theme"){
@@ -17,6 +21,7 @@ function changeText () {
 }
 buttonRef.addEventListener("click", changeText);
 
+//Clicking "Cancel" button will hide text area, save and cancel button
 const save = document.querySelector('.save')
 const textArea = document.querySelector('.textarea')
 const cancel = document.querySelector('.cancel')
@@ -27,6 +32,7 @@ cancel.addEventListener("click", () => {
     cancel.style.display = 'none';
 });
 
+// Pressing "New Note" button would make them visible again
 const newNote = document.querySelector('.notes');
 let isTextAreaVisible = false;
 newNote.addEventListener("click", () => {
@@ -38,3 +44,18 @@ newNote.addEventListener("click", () => {
     cancel.style.display = 'inline-block';
     }
 })
+
+// Declare an array (notesArray)
+// Make sure this variable is declared outside of any function
+const notesArray = [
+    {title: "note one", body: "this is my first note"}
+]
+
+// Show a prompt to the user and ask for the title of the note
+save.addEventListener("click", () => {
+    const title = prompt("Please enter a title for your notes:");
+    const body = textArea.value;
+    const note = {title, body};
+    notesArray.push(note);
+})
+
